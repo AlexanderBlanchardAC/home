576ede3b38f591b085db95f1000a8b7b85bc22b7
AlexanderBlanchardAC <124055686+AlexanderBlanchardAC@users.noreply.github.com>
2023-04-03 00:20:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './Pages/Home/Home';
+import Home from './Pages/Home/Home.jsx';
 import Quiz from './Pages/Quiz/Quiz';
 import Score from './Pages/Score/Score';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
